69e0c2eba63244be4ef13f7e78b59a3b8a3e1086
Dismefront <58440608+Dismefront@users.noreply.github.com>
2022-11-24 21:17:21 +0300

    backend init

---
+++ backend/hack/src/main/java/spokers/hack/controller/Controller.java
@@ -0,0 +1,15 @@
+package spokers.hack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "start_page";
+    }
+
+}
