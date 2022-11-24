cf4d7f1db68245f28b24ec7cf0e1b01a41a5fb88
Dead <space.gee.z@gmail.com>
2022-11-24 20:22:11 +0300

    add button in form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -4,12 +4,13 @@
     <div className="grid p-10 space-y-10 border-green-300 border-2 shadow-lg place-items-center rounded-lg w-fit ">
         <div className="text-green-300 text-center text-2xl"> Enter url</div>
         <div className=" place-self-center">
-            <form className="w-96">
+            <form className="w-96 grid place-items-center space-y-10">
                 <input
                 type={"url"}
                 placeholder="git repo url"
                 className="w-full rounded text-center"
                 />
+                <button className="border-2 rounded-full p-1 px-4 shadow-lg text-green-300 border-green-300">Analyze</button>
             </form>
         </div> 
     </div>);
