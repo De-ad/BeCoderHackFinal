74d5e8c0c85869b65d2765a9e0bd054858770e25
Dead <space.gee.z@gmail.com>
2022-11-25 07:48:06 +0300

    fix table position

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -4,7 +4,6 @@ import { Results } from "./Results";
 function MainPage(){
     return(
         <div className=" grid justify-items-center p-10 font-thin space-y-10"> 
-            {/* <Form /> */}
             <Results />
         </div>
     );
--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -37,29 +37,31 @@ export class Results extends Component {
       const line = "This web application checks which files from git have high risk of errors and who is the best reviewer to fix commit"
       return (
         <div>
+          <div className="grid space-y-10 place-items-center">
           <div className="grid p-10 space-y-10 border-green-300 border-2 shadow-lg place-items-center rounded-lg w-fit">
-        <div className="text-green-300 text-xl">{line}</div>
-        <div className="text-green-300 text-center text-2xl"> Enter url</div>
-        <div className=" place-self-center">
-            <form className="w-96 grid place-items-center space-y-10" onSubmit={this.handlesbm}>
-                <input id="inpurl"
-                type={"url"}
-                placeholder="git repo url"
-                className="w-full rounded text-center"
-                // onChange={handleFormChange()}
-                // onSubmit={handleSubmit()}
-                />
-                {/* <button className="border-2 rounded-full p-1 px-4 shadow-lg text-green-300 border-green-300">Analyze</button> */}
-                <div className="flex items-center justify-center pb-8"><LoadButton
-                  onLoad={this.onLoadTables}
-                  title="Analyze"
-                /></div>
-            </form>
-        </div> 
-    </div>
+            <div className="text-green-300 text-xl">{line}</div>
+            <div className="text-green-300 text-center text-2xl"> Enter url</div>
+            <div className=" place-self-center">
+                <form className="w-96 grid place-items-center space-y-10" onSubmit={this.handlesbm}>
+                    <input id="inpurl"
+                    type={"url"}
+                    placeholder="git repo url"
+                    className="w-full rounded text-center"
+                    // onChange={handleFormChange()}
+                    // onSubmit={handleSubmit()}
+                    />
+                    {/* <button className="border-2 rounded-full p-1 px-4 shadow-lg text-green-300 border-green-300">Analyze</button> */}
+                    <div className="flex items-center justify-center pb-8"><LoadButton
+                      onLoad={this.onLoadTables}
+                      title="Analyze"
+                    /></div>
+                </form>
+            </div> 
+          </div>
           
           <div className="tables">
             <CommitTable commits={this.state.commits} />
+          </div>
           </div>
         </div>
       );
