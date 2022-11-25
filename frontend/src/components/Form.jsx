ff4537c3d3b48572d2f4d4db5908f2517e3351e3
Dead <space.gee.z@gmail.com>
2022-11-25 03:20:29 +0300

    add message

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,9 +1,11 @@
  import { LoadButton } from "./LoadButton";
 import { Results } from "./Results";
  export default function Form(){
+    const line = "This web application checks which files from git have high risk of errors and who is the best reviewer to fix commit"
     
     return(
-    <div className="grid p-10 space-y-10 border-green-300 border-2 shadow-lg place-items-center rounded-lg w-fit ">
+    <div className="grid p-10 space-y-10 border-green-300 border-2 shadow-lg place-items-center rounded-lg w-fit">
+        <div className="text-green-300 text-xl">{line}</div>
         <div className="text-green-300 text-center text-2xl"> Enter url</div>
         <div className=" place-self-center">
             <form className="w-96 grid place-items-center space-y-10">
