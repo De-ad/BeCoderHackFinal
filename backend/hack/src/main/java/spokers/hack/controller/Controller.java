0f7dd6dab3d6b044a201b91f800f814abace15fb
SamBridgess <81350195+SamBridgess@users.noreply.github.com>
2022-11-25 10:20:59 +0300

    add model

--- backend/hack/src/main/java/spokers/hack/controller/Controller.java
+++ backend/hack/src/main/java/spokers/hack/controller/Controller.java
@@ -1,7 +1,13 @@
 package spokers.hack.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.web.bind.annotation.*;
+import spokers.hack.controller.calculations.RetList;
+import spokers.hack.controller.calculations.Runner;
+
 import java.io.*;
+import java.util.ArrayList;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
@@ -11,19 +17,27 @@ public class Controller {
 
     @GetMapping
     public String getCommits(@RequestParam(value = "url", defaultValue = "") String value) throws IOException {
-//        python here
+
+//        value это юрл котрый принимает парсер
+//        он парсит данные. проверяет по критериям коммиты. результат пишет в файл.цсв этот файл принимается на фронте и
+//        и выводится в таблицу пример файла есть в телеге. нужно поменять путь сверху на относительный чтбы работало норм
         System.out.println(value);
-        try {
-            BufferedReader breader = new BufferedReader(new FileReader(file));
-            String res = "";
-            String st = "";
-            while ((st = breader.readLine()) != null)
-                res += st + "\n";
-            return res;
-        }
-        catch (Exception e){
-            throw e;
-        }
+        ArrayList<RetList> ret = Runner.run(value);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(ret);
+        System.out.println(json);
+        return json;
+//        try {
+//            BufferedReader breader = new BufferedReader(new FileReader(file));
+//            String res = "";
+//            String st = "";
+//            while ((st = breader.readLine()) != null)
+//                res += st + "\n";
+//            return ret;
+//        }
+//        catch (Exception e){
+//            throw e;
+//        }
     }
 
 }
